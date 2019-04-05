3b9a9111a9c996346bc4e815286160e31a6a5628
gehriben <gehriben@students.zhaw.ch>
2019-04-05 23:19:10 +0200

    Create two Components one for the issue page and one to create a new issue

---
+++ brigemi-frontend/src/app/Pages/create-issue/create-issue.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-issue',
+  templateUrl: './create-issue.component.html',
+  styleUrls: ['./create-issue.component.css']
+})
+export class CreateIssueComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- brigemi-frontend/src/app/app.module.ts
+++ brigemi-frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { IssuesComponent } from './Pages/issues/issues.component';
+import { CreateIssueComponent } from './Pages/create-issue/create-issue.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IssuesComponent,
+    CreateIssueComponent
   ],
   imports: [
     BrowserModule,
