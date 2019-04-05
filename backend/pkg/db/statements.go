70999144fec1c368d152381c2ccbd2cb22b3d6ea
info@ninetyfour.ch <mj@localhost.localdomain>
2019-04-05 23:21:06 +0200

    update basic auth with default user

--- backend/pkg/db/statements.go
+++ backend/pkg/db/statements.go
@@ -1,18 +1,30 @@
+// Package db provides the functions and constants to select / insert
+// values into the database
+// notes for the future:
+// LastInsertRowID() returns int64 of the last successful insert statement
 package db
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/bvinc/go-sqlite-lite/sqlite3"
 )
 
-var (
-	test *sqlite3.Stmt
-)
-
 const (
+	// create table statements
 	createUserTable     string = `create table if not exists user(id int primary key, name text, password text);`
 	createFileTable     string = `create table if not exists file(id int primary key, location text, documentid int, foreign key(documentid) references document(id));`
 	createNoteTable     string = `create table if not exists note(id int primary key, content text, fileid int, foreign key(fileid) references file(id));`
 	createDocumentTable string = `create table if not exists document(id int primary key, name text, location text);`
+	// insert statements
+	insertUser     string = `insert into user(name, password) values (?,?);`
+	insertFile     string = `insert into file(location, documentid) values (?,?);`
+	insertNote     string = `insert into note(content, fileid) values (?,?);`
+	insertDocument string = `insert into document(name, location) values (?,?);`
+	// select statements
+	selectUsers        string = `select user.name, user.password from user;`
+	selectFileLocation string = `select file.location from file where file.id = ?;`
 )
 
 // CreateTables creates the tables if they don't exist
@@ -33,7 +45,38 @@ func CreateTables(conn *sqlite3.Conn) error {
 	return err
 }
 
-// CreateStmts prepares all the statements
-func CreateStmts(conn *sqlite3.Conn) {
-	// stmt, err := conn.Prepare()
+// GetUsers returns a map with all the users which
+// are in the database
+func GetUsers(conn *sqlite3.Conn) (map[string]string, error) {
+	users := make(map[string]string)
+	stmt, err := conn.Prepare(selectUsers)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	for {
+		hasRow, err := stmt.Step()
+		if err != nil {
+			log.Fatal(err)
+		}
+		if !hasRow {
+			// The query is finished
+			break
+		}
+
+		// Use Scan to access column data from a row
+		var name string
+		var password string
+		err = stmt.Scan(&name, &password)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("name: %s, password: %s\n", name, password)
+		users[name] = password
+	}
+	if len(users) == 0 {
+		users = map[string]string{"user": "password"}
+	}
+	return users, err
 }
--- backend/pkg/rtr/routes.go
+++ backend/pkg/rtr/routes.go
@@ -1,15 +1,27 @@
 package rtr
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 // InitRouter initialises the routes
-func InitRouter() *gin.Engine {
+func InitRouter(users map[string]string) *gin.Engine {
 	router := gin.Default()
+
+	authorized := router.Group("/", gin.BasicAuth(users))
 
 	// add routes
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
+		})
+	})
+
+	authorized.GET("/secret", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"secret": "The secret ingredient to the BBQ sauce is stiring it in an old whiskey barrel.",
 		})
 	})
 
