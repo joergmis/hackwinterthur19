4ce4a988ed6c1a11047cef155226c902696fccb2
info@ninetyfour.ch <mj@localhost.localdomain>
2019-04-05 21:51:55 +0200

    init backend

---
+++ backend/cmd/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/bvinc/go-sqlite-lite/sqlite3"
+)
+
+func main() {
+	conn, err := sqlite3.Open("./../database/database.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	fmt.Println("hello world")
+}
