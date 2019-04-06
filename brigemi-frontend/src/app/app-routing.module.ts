5fe70cdd67a453a8d959553262ec747599eb45ee
gehriben <gehriben@students.zhaw.ch>
2019-04-06 15:39:57 +0200

    Create HTML Mockup for login

--- brigemi-frontend/src/app/app-routing.module.ts
+++ brigemi-frontend/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { IssuesComponent } from './Pages/issues/issues.component';
 import { CreateIssueComponent } from './Pages/create-issue/create-issue.component';
 import { ShowIssueComponent } from './Pages/show-issue/show-issue.component';
+import { LoginComponent } from './Pages/login/login.component';
 
 const routes: Routes = [
   { path: '', component: IssuesComponent },
   { path: 'createIssues', component: CreateIssueComponent },
-  { path: 'showIssue', component: ShowIssueComponent }
+  { path: 'showIssue', component: ShowIssueComponent },
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
--- brigemi-frontend/src/app/app.module.ts
+++ brigemi-frontend/src/app/app.module.ts
@@ -47,13 +47,15 @@ import { AppComponent } from './app.component';
 import { IssuesComponent } from './Pages/issues/issues.component';
 import { CreateIssueComponent } from './Pages/create-issue/create-issue.component';
 import { ShowIssueComponent } from './Pages/show-issue/show-issue.component';
+import { LoginComponent } from './Pages/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IssuesComponent,
     CreateIssueComponent,
-    ShowIssueComponent
+    ShowIssueComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
