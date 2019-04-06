193e8257d192c77b888abee7f6fe9cac72b32264
Mischa Jörg <info@ninetyfour.ch>
2019-04-06 14:34:30 +0200

    update

--- brigemi-frontend/src/app/Pages/show-issue/show-issue.component.ts
+++ brigemi-frontend/src/app/Pages/show-issue/show-issue.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
+import { RestService } from 'src/app/Services/rest-service';
+
+import { Issue } from '../../Objects/issue';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-show-issue',
@@ -7,10 +11,28 @@ import { Router } from '@angular/router';
   styleUrls: ['./show-issue.component.css']
 })
 export class ShowIssueComponent implements OnInit {
+  public issue : Issue;
 
-  constructor(private router : Router) { }
+  private restService;
+
+  constructor(private http: HttpClient, private router : Router, private ar : ActivatedRoute) {
+
+    this.issue = new Issue('','');
+    this.restService = new RestService(this.http);
+
+    ar.queryParams.subscribe(params => {
+      this.issue.setIssueID(params['id']);
+    })
+
+    this.restService.get(this.issue.getId(), "issues/").subscribe(
+      data => { this.issue = data; },
+      err => { console.log(err); }
+    );
+
+  }
 
   ngOnInit() {
+    
   }
 
   redirectFunction(url : string, id : number) : void {
